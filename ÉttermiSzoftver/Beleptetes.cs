ff994fea5ec2a235047742a6cc1e7f328e605bbf
gergo <gergo@GERI-PC>
2018-04-16 09:18:01 +0200

    Login + rendeles

---
+++ ÉttermiSzoftver/Beleptetes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÉttermiSzoftver
+{
+    public class Beleptetes
+    {
+        private static int _Felhasznalo; // Ebben tároljuk a felhasználó azonosítóját
+
+        private static bool _admin; // Ebben tároljuk a felhasználó azonosítóját
+
+        public int Felhasz
+        {
+            get
+            {
+                return _Felhasznalo;
+            }
+            set
+            {
+                _Felhasznalo = value;
+            }
+        }
+
+        public bool Admin
+        {
+            get
+            {
+                return _admin;
+            }
+
+            set
+            {
+                _admin = value;
+            }
+        }
+    }
+}
+}
--- ÉttermiSzoftver/ucLogin.xaml.cs
+++ ÉttermiSzoftver/ucLogin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ÉttermiSzoftver_DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,24 +21,73 @@ namespace ÉttermiSzoftver
     /// </summary>
     public partial class ucLogin : UserControl
     {
+        private cnÉttermiSzoftver cnEttermiSzoftver = new cnÉttermiSzoftver();
+        private Beleptetes b = new Beleptetes();
+        private string n;
+
+        public delegate void OnBelep();
+        public static event OnBelep OnLogin;
+
         public ucLogin()
         {
             InitializeComponent();
         }
 
+        // -----------------------------------------------------------------------------------------------------
+        // Regisztráció - A gombra kattintva elmentjük a felhasználót az adatbázisba
+        // -----------------------------------------------------------------------------------------------------
         private void btBelepes_Click(object sender, RoutedEventArgs e)
         {
+            string f = tbNev.Text; // Felhasznalo nev
+            string j = pbJelszo.Password; // Jelszó
 
+           /* var Fe = from x in cnEttermiSzoftver.enSzemelyek // Megkeressük az adatbázisban a felhasználót
+                     where f == x.BejelentkezesiNev
+                     select x.BejelentkezesiNev;
+
+            var Je = from x in cnEttermiSzoftver.enSzemelyek // Megkeressük az adatbázisban a jelszót
+                     where f == x.BejelentkezesiNev
+                     select x.Jelszo;
+*/
+            // ADMIN belépés
+            if (f == "admin")
+            {
+                if (j == "admin")
+                {
+                    //b.Felhasz = 1;
+                    b.Admin = true;
+
+                    if (OnLogin != null)
+                        OnLogin();
+                    MessageBox.Show("Belépés sikeres!\n\nÜdvözlünk ADMIN!");
+                }
+                else MessageBox.Show("Hibás felhasználónév vagy jelszó!");
+            }
+
+            // Felhasználó belépés
+            else if (Fe.FirstOrDefault() == f)
+            {
+                if (Je.FirstOrDefault() == j)
+                {
+                    var Fe2 = from x in cnEttermiSzoftver.enSzemelyek // Megkeressük az adatbázisban a felhasználót
+                              where f == x.BejelentkezesiNev
+                              select x.Id;
+
+                    b.Admin = false;
+
+                    OnLogin();
+
+                    b.Felhasz = (from x in cnEttermiSzoftver.enSzemelyek where f == x.Nev select x.Id).FirstOrDefault(); // Felhasználó ID lekérdezése
+
+                    n = (from x in cnEttermiSzoftver.enSzemelyek where f == x.Nev select x.Pozicio).FirstOrDefault(); // Felhasználó nevének lekérdezése
+                    MessageBox.Show("Belépés sikeres!\n\nÜdvözlünk " + n);
+
+                }
+                else MessageBox.Show("Hibás felhasználónév vagy jelszó!");
+            }
+
+            else MessageBox.Show("Hibás felhasználónév vagy jelszó!");
         }
 
-        private void btMegse_Click(object sender, RoutedEventArgs e)
-        {
-            ucLogin.Visibility = Visibility.Hidden;
-        }
-
-        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-
-        }
     }
 }
